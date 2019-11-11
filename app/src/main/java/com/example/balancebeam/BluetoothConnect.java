05a21c0b74d6b7c7ef6b665cb9f2d4a9df503135
Saulius Baltramonaitis <msnfan234@gmail.com>
2019-11-11 21:07:16 +0000

    add bluetooth detection

--- app/src/main/java/com/example/balancebeam/BluetoothConnect.java
+++ app/src/main/java/com/example/balancebeam/BluetoothConnect.java
@@ -1,13 +1,33 @@
 package com.example.balancebeam;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BluetoothConnect extends AppCompatActivity {
 
+    public static final int REQUEST_ENABLE_BT = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bluetooth_connect);
+        InitBluetooth();
     }
+
+    public void InitBluetooth() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothAdapter == null) {
+            Intent noBtIntent = new Intent(this, NoBluetooth.class);
+            startActivity(noBtIntent);
+        }
+        else if(!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/balancebeam/NoBluetooth.java
@@ -0,0 +1,13 @@
+package com.example.balancebeam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoBluetooth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.no_bluetooth);
+    }
+}
