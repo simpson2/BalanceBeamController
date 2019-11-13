69e1f263d19b03f8a04d560aed55414247e9552a
Saulius Baltramonaitis <msnfan234@gmail.com>
2019-11-13 16:08:32 +0000

    commit

--- app/src/main/java/com/example/balancebeam/BluetoothConnect.java
+++ app/src/main/java/com/example/balancebeam/BluetoothConnect.java
@@ -1,23 +1,35 @@
 package com.example.balancebeam;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class BluetoothConnect extends AppCompatActivity {
 
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     public static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bluetooth_connect);
-        InitBluetooth();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+        initBluetooth();
+
+        TextView view = findViewById(R.id.debug);
+
     }
 
-    public void InitBluetooth() {
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    public void initBluetooth() {
 
         if(bluetoothAdapter == null) {
             noBt();
@@ -34,7 +46,30 @@ public class BluetoothConnect extends AppCompatActivity {
             if(resultCode == RESULT_CANCELED) {
                 noBt();
             }
+            else {
+                bluetoothAdapter.startDiscovery();
+            }
         }
+    }
+
+    final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceAddress = device.getAddress();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(receiver);
     }
 
     public void noBt() {
