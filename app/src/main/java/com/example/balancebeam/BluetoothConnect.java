5d1d5d6894cf434a3bb65f42f41ac20c359dde72
Saulius Baltramonaitis <msnfan234@gmail.com>
2019-11-28 00:30:05 +0000

    commit

--- app/src/main/java/com/example/balancebeam/BluetoothConnect.java
+++ app/src/main/java/com/example/balancebeam/BluetoothConnect.java
@@ -9,12 +9,10 @@ import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Set;
